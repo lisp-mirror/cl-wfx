6659c135e9e2f71f6dc66f9033935a79cc5e29da
Phil Marneweck <haragx@gmail.com>
2018-01-16 21:47:38 +0200

    Stop replace all from crashing on nil

--- common.lisp
+++ common.lisp
@@ -73,17 +73,18 @@ Dont set manually use with-system macro.")
 (defun replace-all (string part replacement &key (test #'char=))
 "Returns a new string in which all the occurences of the part 
 is replaced with replacement."
-    (with-output-to-string (out)
-      (loop with part-length = (length part)
-            for old-pos = 0 then (+ pos part-length)
-            for pos = (search part string
-                              :start2 old-pos
-                              :test test)
-            do (write-string string out
-                             :start old-pos
-                             :end (or pos (length string)))
-            when pos do (write-string replacement out)
-	 while pos)))
+(when string
+  (with-output-to-string (out)
+    (loop with part-length = (length part)
+       for old-pos = 0 then (+ pos part-length)
+       for pos = (search part string
+			 :start2 old-pos
+			 :test test)
+       do (write-string string out
+			:start old-pos
+			:end (or pos (length string)))
+       when pos do (write-string replacement out)
+       while pos))))
 
 #|
 (defun plural-name (name)
